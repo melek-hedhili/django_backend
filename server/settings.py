6d32d477d2170fdaf6d7f5b64195b2b634ef42c9
melek <melekhedhili7@gmail.com>
2022-07-02 17:23:44 +0100

    updated

--- server/settings.py
+++ server/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'server/treated_image'),
-    os.path.join(BASE_DIR, 'server/treated_video'),
-    os.path.join(BASE_DIR, 'server'),
+    os.path.join(BASE_DIR, 'treated_image'),
+    os.path.join(BASE_DIR, 'treated_video'),
+    os.path.join(BASE_DIR, ''),
 )
 
 # Default primary key field type
